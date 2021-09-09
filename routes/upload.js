e00462b641075ee85d2162da398c268fa150c86d
VayerMaking <36741716+VayerMaking@users.noreply.github.com>
2021-09-09 13:54:06 +0300

    add project files

---
+++ routes/upload.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const path = require('path');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('upload', { title: 'Upload Page' });
+});
+
+router.post('/', (req, res) => {
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    const file = req.files.uploadFile;
+    const extension = path.extname(file.name);
+    const allowedExtensions = ['.csv', '.xls', '.xlsx'];
+
+    if (!allowedExtensions.includes(extension)) {
+        return res.status(422).send("Invalid File Format");
+    }
+    return res.status(200).send('success');
+});
+
+module.exports = router;
